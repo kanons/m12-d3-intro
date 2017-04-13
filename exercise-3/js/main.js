d9d9c3981181ae9c79314a4f70c3ae8b289c3bcf
Kanon Shibata <kanon622@me.com>
2017-04-13 10:27:18 -0700

    Add exercise 3 code

--- exercise-3/js/main.js
+++ exercise-3/js/main.js
@@ -26,31 +26,58 @@ var exam3 = [
 // You'll have to wait for you page to load to assign events to the elements created in your index.html file
 $(function() {
   // Select SVG
+  var svg = d3.select('#my-svg').attr('height', 200);
 
   // Reusable draw function
+  var draw = function(data){
 
     // Bind data to selection of rects in your svg
-
+    var rects = svg.selectAll("rect").data(data, function(d) {return d.id});
 
     // Enter rect elements
-
+    rects.enter()
+        .append('rect')
+        .attr('width', 0)
+        .attr('y', function(d) {return d.id*40})
+        .attr('x', 20)
+        .attr('height', 30)
+        .merge(rects)
+        .transition()
+        .duration(500)
+        .attr('width', function(d) {return d.grade});
 
     // Entering and updating elements rects
-
+  
 
     // Transition a remove for exiting elements
-
+    rects.exit()
+         .transition()
+         .duration(500)
+         .attr('width', 0)
+         .remove();
 
     // Perform the same data-binding for text
-
+    var texts = svg.selectAll('text').data(data, function(d) {return d.id});
 
     // enter elements
-
+    texts.enter()
+         .append('text')
+         .merge(texts)
+         .attr('y', function(d) {return d.id*40+19})
+         .text(function(d) {return d.student})
+         .style('fill', 'white')
+         .attr('x', 23);
 
     // Entering and updating elements rects
 
 
     // Transition a remove for exiting elements
+    texts.exit()
+         .transition()
+         .delay(500)
+         .remove();
+  };
+  
     $('button').on('click', function() {
       var btn = $(this).attr("id");
       switch(btn){
